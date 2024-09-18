7aef43fb974926a3ec9e00aab28e560dd7a4030a
Huber_HaYu <104569900+HuberHaYu@users.noreply.github.com>
2024-09-18 23:00:13 +0800

    Update service.sh

--- service.sh
+++ service.sh
@@ -18,7 +18,10 @@ wait_until_login() {
         _path_max="/sys/class/backlight/panel0-backlight/max_brightness"
         cur_brightness=$(cat "$_path")
         if [ "$cur_brightness" -gt 1500 ]; then
+            settings put system screen_brightness_mode 0
             echo 3800 > "$_path"
+        else
+            settings put system screen_brightness_mode 1
         fi
         sleep 2
     done
